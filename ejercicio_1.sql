9c27337abedc957ed2566cc1587ae7753a24a0d4
Anayeli Karina <anayelisainos664@gmail.com>
2022-01-30 19:10:16 -0600

    merge

--- ejercicio_1.sql
+++ ejercicio_1.sql
@@ -7,7 +7,12 @@ SELECT * FROM pokemon_species
 -- 3
 SELECT * FROM types 
 
+<<<<<<< HEAD
 -- 4 
 SELECT * FROM pokemon_types
+=======
+--1 con develop
+SELECT height FROM pokemon
+>>>>>>> develop
 
 
